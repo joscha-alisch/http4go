9c4205becf815b7d56c251bb3e99520e77633781
Joscha Alisch <joscha.alisch@gmail.com>
2025-09-09 14:39:12 +0200

    Add basic uri

--- filters/set_host_from.go
+++ filters/set_host_from.go
@@ -1,17 +1,23 @@
 package filters
 
 import (
+	"fmt"
+
 	"github.com/joscha-alisch/http4go/http"
 	"github.com/joscha-alisch/http4go/http/uri"
 )
 
 func SetHostFrom(u uri.Uri) http.Filter {
-
+	return func(next http.Handler) http.Handler {
+		return func(r http.Request) (http.Response, error) {
+			host := u.GetHost()
+			port := u.GetPort()
+			hostPort := host
+			if port != 0 {
+				hostPort = fmt.Sprintf("%s:%d", host, port)
+			}
+			return next(r.Uri(r.GetUri().Scheme(u.GetScheme()).Host(u.GetHost()).Port(u.GetPort())).
+				ReplaceHeader("Host", hostPort))
+		}
+	}
 }
-
-/* fun SetHostFrom(uri: Uri): Filter = Filter { next ->
-{
-next(it.uri(it.uri.scheme(uri.scheme).host(uri.host).port(uri.port))
-.replaceHeader("Host", "${uri.host}${uri.port?.let { port -> ":$port" } ?: ""}"))
-}
-}*/
--- http/uri/uri.go
+++ http/uri/uri.go
@@ -6,21 +6,68 @@ var authorityRegex = regexp.MustCompile("(?:([^@]+)@)?([^:]+)(?::([\\d]+))?")
 var rfc3986Regex = regexp.MustCompile("^(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*)(?:\\?([^#]*))?(?:#(.*))?")
 
 type Uri struct {
-	Scheme   string
-	Host     string
-	Port     int
-	Path     string
-	Query    string
-	Fragment string
+	scheme   string
+	host     string
+	port     int
+	path     string
+	query    string
+	fragment string
 }
 
-func Of(uri string) (Uri, error) {
-	groups := rfc3986Regex.FindStringSubmatch(uri)
-	authority := groups[2]
-	authorityGroups := authorityRegex.FindStringSubmatch(authority)
+func NewUri() Uri {
+	return Uri{}
+}
 
-	return Uri{
-		Scheme: groups[1],
-		Host:   authorityGroups[2],
-	}, nil
+func (u Uri) Scheme(scheme string) Uri {
+	u.scheme = scheme
+	return u
+}
+
+func (u Uri) Host(host string) Uri {
+	u.host = host
+	return u
+}
+
+func (u Uri) Port(port int) Uri {
+	u.port = port
+	return u
+}
+
+func (u Uri) Path(path string) Uri {
+	u.path = path
+	return u
+}
+
+func (u Uri) Query(query string) Uri {
+	u.query = query
+	return u
+}
+
+func (u Uri) Fragment(fragment string) Uri {
+	u.fragment = fragment
+	return u
+}
+
+func (u Uri) GetScheme() string {
+	return u.scheme
+}
+
+func (u Uri) GetHost() string {
+	return u.host
+}
+
+func (u Uri) GetPort() int {
+	return u.port
+}
+
+func (u Uri) GetPath() string {
+	return u.path
+}
+
+func (u Uri) GetQuery() string {
+	return u.query
+}
+
+func (u Uri) GetFragment() string {
+	return u.fragment
 }
